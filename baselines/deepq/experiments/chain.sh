bcf3c9e4588dc843129618cffb379682de0b3d7e
Tim Elfrink <t.elfrink@mobiquityinc.com>
2018-07-02 11:10:50 +0200

    fix

--- baselines/deepq/experiments/chain.sh
+++ baselines/deepq/experiments/chain.sh
@@ -3,7 +3,7 @@ for seed in 1 2 3
 do
     for i in 10 20 30 40 50 60 70 80 90 100;
     do
-	    for exp in "" ""--noisy=1" "--greedy=1" "--bootstrap=1";
+	    for exp in "" "--noisy=1" "--greedy=1" "--bootstrap=1";
 	    do
             python -m baselines.deepq.experiments.train_chain --n=$i $exp --seed=$seed
         done
